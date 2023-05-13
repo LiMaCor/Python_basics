cbcced5f2105883ba2133e25e791ab8ef4f60b39
LiMaCor <julian.itsa8@gmail.com>
2023-05-13 20:15:48 +0200

    Init git

---
+++ 1 - Learn the basics/Hello World.py
@@ -0,0 +1,8 @@
+# Use the function print to print a message in the commmand prompt
+# print function adds a new line at the end
+print("Hello, world")
+
+x = 1
+if x == 1:
+    # Python uses indentations for blocks isntead of curly braces
+    print("x is 1.")
---
+++ 1 - Learn the basics/Variables and types.py
@@ -0,0 +1,31 @@
+# Python supports two types of numbers: integers and floating point numbers (and other complex types)
+myint = 7
+print(myint)
+
+# Use one of these notations to define a floating point number
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+# Strings can be defined using either single o double quotation marks
+# Double quotes makes easy include apostrophes on strings
+mystring = 'Hello single quote mark'
+print(mystring)
+mystring = "Hello double quote mark's"
+print(mystring)
+
+# Simple operators can be used on numbers and strings
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "Hello"
+world = "world"
+helloworld = hello + ", " + world
+print(helloworld)
+
+# Assignments can be done on more than one variable on the same line
+a, b = 3, 4
+print(a, b)
